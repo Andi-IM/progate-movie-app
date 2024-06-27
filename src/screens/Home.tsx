8fe46496d06018431a91a53dc334d76db131e9c1
Andi Irham <andi.irhamm@gmail.com>
2024-06-27 15:44:21 +0700

    update develop home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,20 +1,54 @@
 import React from 'react'
-import {Button, StyleSheet, Text, View} from 'react-native'
+import {ScrollView, StatusBar, StyleSheet, View} from 'react-native'
+import {MovieListProps} from "../types/app";
+import MovieList from "../components/movies/MovieList";
+
+const movieList: MovieListProps[] = [
+    {
+        title: 'Now Playing in Theater',
+        path: 'movie/now_playing?language=en-US&page=1',
+        coverType: 'backdrop',
+    },
+    {
+        title: 'Upcoming Movies',
+        path: 'movie/upcoming?language=en-US&page=1',
+        coverType: 'poster',
+    },
+    {
+        title: 'Top Rated Movies',
+        path: 'movie/top_rated?language=en-US&page=1',
+        coverType: 'poster',
+    },
+    {
+        title: 'Popular Movies',
+        path: 'movie/popular?language=en-US&page=1',
+        coverType: 'poster',
+    },
+]
 
 export default function Home({navigation}: any): JSX.Element {
     return (
-        <View style={styles.container}>
-            <Text>Home</Text>
-            <Button title="PERGI KE MOVIE DETAIL"
-                    onPress={() => navigation.navigate('DETAIL')}/>
-        </View>
+        <ScrollView>
+            <View style={styles.container}>
+                {movieList.map((movieList) => (
+                    <MovieList title={movieList.title}
+                               path={movieList.path}
+                               coverType={movieList.coverType}
+                               key={movieList.title}
+                    />
+                ))}
+                <StatusBar translucent={false}/>
+            </View>
+
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        marginTop: StatusBar.currentHeight ?? 32,
         alignItems: 'center',
         justifyContent: 'center',
+        rowGap: 16,
     },
 })
